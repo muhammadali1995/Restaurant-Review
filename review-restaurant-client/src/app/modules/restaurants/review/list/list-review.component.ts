9fcd2b0a15a52683efa22d63b3825ddc470c5e09
mtoshpulatov1995@gmail.com <4377433mtDEV>
2021-02-21 01:26:44 +0500

    reply to review implemented

--- review-restaurant-client/src/app/modules/restaurants/review/list/list-review.component.ts
+++ review-restaurant-client/src/app/modules/restaurants/review/list/list-review.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {RestaurantModel} from '../../../../models/restaurant.model';
-import {faPencilAlt, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {faPencilAlt, faTrashAlt, faReply} from '@fortawesome/free-solid-svg-icons';
 import {ReviewModel} from '../../../../models/review.model';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {DeleteReviewComponent} from '../delete/delete-review.component';
+import {ReplyReviewComponent} from "../reply/reply-review.component";
 
 @Component({
   selector: 'app-list-review',
@@ -15,6 +16,7 @@ export class ListReviewComponent implements OnInit {
   @Input() restaurant: RestaurantModel;
   faTrash = faTrashAlt;
   faPencil = faPencilAlt;
+  faReply = faReply;
 
   constructor(private modalService: NgbModal) {
   }
@@ -30,6 +32,17 @@ export class ListReviewComponent implements OnInit {
     modal.result.then(res => {
       if (res === 'success') {
         this.restaurant.reviews.splice(index, 1);
+      }
+    });
+  }
+
+  reply(review: ReviewModel) {
+    const index = this.restaurant.reviews.indexOf(review);
+    const modal = this.modalService.open(ReplyReviewComponent);
+    modal.componentInstance.review = review;
+    modal.result.then(res => {
+      if (res && res.id) {
+        this.restaurant.reviews[index] = res;
       }
     });
   }
--- review-restaurant-client/src/app/modules/restaurants/review/reply/reply-review.component.ts
+++ review-restaurant-client/src/app/modules/restaurants/review/reply/reply-review.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {ReviewModel} from '../../../../models/review.model';
+import {ReviewService} from '../review.service';
 
 @Component({
   selector: 'app-reply-review',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReplyReviewComponent implements OnInit {
 
-  constructor() { }
+  @Input() review: ReviewModel;
+
+  error: string;
+  form: FormGroup;
+  saving: boolean;
+
+  constructor(private activeModal: NgbActiveModal,
+              private fb: FormBuilder,
+              private reviewService: ReviewService) {
+    this.form = this.fb.group({
+      reply: [null, [Validators.required, this.noWhitespaceValidator]]
+    });
+  }
 
   ngOnInit(): void {
+
+    this.reviewService.fetchOne(this.review.id)
+      .subscribe(
+        (review: ReviewModel) => {
+          this.review = review;
+        },
+
+        err => this.error = err.error.message);
+  }
+
+
+  get reply() {
+    return this.form.get('reply');
+  }
+
+  get getReplyError() {
+    if (this.reply.hasError('required')) {
+      return 'Comment is required';
+    }
+    return this.reply.hasError('whitespace') ? 'Reply can not be whitespaces' : '';
+
+  }
+
+
+  public noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : {whitespace: true};
+  }
+
+  onClose() {
+    this.activeModal.close();
+  }
+
+  onSubmit() {
+    this.saving = true;
+    const request = this.form.value;
+    this.reviewService.update(this.review.id, request)
+      .subscribe((updatedReview: ReviewModel) => {
+        this.activeModal.close(updatedReview);
+      }, err => this.error = err.error.message);
   }
 
 }
